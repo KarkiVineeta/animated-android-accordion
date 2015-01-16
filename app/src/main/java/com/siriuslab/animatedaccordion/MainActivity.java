f2b08f8a479dd23b90b72f0cd7bea97ba5cd4280
Alexander Kolesnikov <sundraw@gmail.com>
2015-01-16 19:42:10 +0000

    Static version.

--- app/src/main/java/com/siriuslab/animatedaccordion/MainActivity.java
+++ app/src/main/java/com/siriuslab/animatedaccordion/MainActivity.java
@@ -1,19 +1,76 @@
 package com.siriuslab.animatedaccordion;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity
 {
+    View section1, section2, section3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        section1 = findViewById(R.id.section1);
+        section2 = findViewById(R.id.section2);
+        section3 = findViewById(R.id.section3);
+
+        View header1 = findViewById(R.id.header1);
+        header1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (section1.getVisibility() == View.GONE)
+                {
+                    section1.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    section1.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        View header2 = findViewById(R.id.header2);
+        header2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (section2.getVisibility() == View.GONE)
+                {
+                    section2.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    section2.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        View header3 = findViewById(R.id.header3);
+        header3.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (section3.getVisibility() == View.GONE)
+                {
+                    section3.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    section3.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 
 
